0e7bbf0c9bc180b485ecc68560dbaba2fb125c16
Alessandro <75728476+dragoa@users.noreply.github.com>
2023-09-06 14:40:38 +0200

    validation test

--- src/main/PDFReportCreation/myProcessor.py
+++ src/main/PDFReportCreation/myProcessor.py
@@ -37,63 +37,38 @@ def sanitize_parameters(params):
         return params
 
 
-
-def validate_pdf_spec(pdf_spec):
-    valid = True
-
-    # Validate filename
-    sFileName = pdf_spec.get("filename", "")
+def validate_parameters(aoParams):
+    # validation of the parameters
+    sFileName = aoParams.get("filename", "")
     if sFileName == "":
-        sFileName = uuid.uuid4()
-        print(f"FileName is not set! Generating a random UUID one... {sFileName}")
-    elif os.path.splitext(sFileName)[1] != ".pdf":
-        print("Filename does not have a '.pdf' extension.")
-        valid = False
+        sFileName = str(uuid.uuid4()) + "report"
+        wasdi.wasdiLog(f"FileName is not set! Generating a random UUID one... {sFileName}")
+        aoParams["filename"] = sFileName  # Updating aoParams with the new filename
+
+    sFileName = f"{sFileName}.pdf" if not sFileName.endswith('.pdf') else sFileName
 
     # Validate cover_page
-    cover_page = pdf_spec.get("cover_page", {})
+    cover_page = aoParams.get("cover_page", {})
     if not cover_page.get("template_image_filename"):
-        print("Cover page template_image_filename is missing.")
-        valid = False
+        wasdi.wasdiLog("Cover page template_image_filename is missing.")
 
     # Validate header
-    header = pdf_spec.get("header", {})
+    header = aoParams.get("header", {})
     if not all(header.get(k) for k in ["title", "logo", "author_name", "company_name"]):
-        print("Header is missing one or more required fields (title, logo, author_name, company_name).")
-        valid = False
+        wasdi.wasdiLog("Header is missing one or more required fields (title, logo, author_name, company_name).")
 
     # Validate chapters
-    chapters = pdf_spec.get("chapters", [])
+    chapters = aoParams.get("chapters", [])
     if len(chapters) == 0:
-        print("Chapters are empty.")
-        valid = False
-
-    for chapter in chapters:
-        if not chapter.get("title"):
-            print("One of the chapter titles is missing.")
-            valid = False
-        if not chapter.get("sections"):
-            print(f"Chapter '{chapter.get('title')}' has no sections.")
-            valid = False
+        wasdi.wasdiLog("Chapters are empty")
 
     # Validate footer
-    footer = pdf_spec.get("footer", {})
+    footer = aoParams.get("footer", {})
     if not all(footer.get(k) for k in ["company_link", "footer_link_alignment", "footer_page_number_alignment"]):
-        print("Footer is missing one or more required fields.")
-        valid = False
+        wasdi.wasdiLog("Footer is missing one or more required fields.")
 
-    # Validate style
-    style = pdf_spec.get("style", {})
-    if not all(style.get(k) for k in ["font_size", "font_family", "font_style", "title_background"]):
-        print("Style is missing one or more required fields.")
-        valid = False
+    return sFileName
 
-    if valid:
-        print("Validation passed.")
-        return True
-    else:
-        print("Validation failed.")
-        return False
 
 def run():
     wasdi.wasdiLog("PDF tutorial v.1.4")
@@ -121,22 +96,10 @@ def run():
     wasdi.wasdiLog("PDF tutorial v.1.4")
     aoParams = wasdi.getParametersDict()
     aoParams = sanitize_parameters(aoParams)
+    sFileName = validate_parameters(aoParams)
 
-    sFileName = aoParams.get("filename", "")
-    if sFileName == "":
-        sFileName = str(uuid.uuid4()) + "report"
-        wasdi.wasdiLog(f"FileName is not set! Generating a random UUID one... {sFileName}")
-        aoParams["filename"] = sFileName  # Updating aoParams with the new filename
-
-    is_valid = validate_pdf_spec(aoParams)  # Calling the validate function here
-
-    if not is_valid:
-        wasdi.wasdiLog("Validation failed. Exiting.")
-        wasdi.updateStatus("ERROR", 0)
-        return
-
-    sFileName = f"{sFileName}.pdf" if not sFileName.endswith('.pdf') else sFileName
     create_pdf(sFileName, aoParams)
+
 
 if __name__ == '__main__':
     wasdi.init("./config.json")
