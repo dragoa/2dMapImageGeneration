2d9bf08f57f2f11927d2f5b947c3e9eff8abc97f
Alessandro <75728476+dragoa@users.noreply.github.com>
2023-10-04 14:54:56 +0200

    refactor

--- src/tests/PDFCreation_Test/Test2.py
+++ src/tests/PDFCreation_Test/Test2.py
@@ -1,55 +1,31 @@
 import unittest
-import uuid
 import json
-import re
-from src.main.PDFReportCreation.myProcessor import validate_parameters
+from io import StringIO
+import sys
+
+# Import the functions from your main script
+from src.main.PDFReportCreation.myProcessor import sanitize_parameters, validate_parameters
 
 
-def read_parameters_from_json(json_file):
-    # Read parameters from the JSON file
-    with open(json_file, "r") as file:
-        params = json.load(file)
-    return params
+class TestParameterValidation(unittest.TestCase):
+    def setUp(self):
+        # Redirect Wasdi log output to a StringIO buffer
+        self.log_output = StringIO()
+        sys.stdout = self.log_output
 
-
-class TestValidateParameters(unittest.TestCase):
+    def tearDown(self):
+        # Restore the standard output
+        sys.stdout = sys.__stdout__
 
     def test_valid_parameters(self):
-        # Read parameters from the JSON file
-        params = read_parameters_from_json("config.json")
+        # Load parameters from a JSON file
+        with open("params.json", "r") as file:
+            params = json.load(file)
 
         # Call the validate_parameters function
         validated_filename = validate_parameters(params)
 
         # Assert that the filename is valid (matches the expected pattern)
-        self.assertTrue(re.match(r"^[a-f0-9-]+report\.pdf$", validated_filename))
-
-    def test_missing_filename(self):
-        # Create a dictionary with missing filename
-        missing_filename_params = {
-            "cover_page": {
-                "template_image_filename": "cover23.jpg",
-                "alignment": "center",
-            }
-        }
-
-        # Call the validate_parameters function
-        validated_filename = validate_parameters(missing_filename_params)
-
-        # Assert that the function generates a random filename with ".pdf" extension
-        self.assertTrue(validated_filename.endswith(".pdf"))
-
-    def test_missing_cover_page(self):
-        # Create a dictionary with missing cover_page
-        missing_cover_page_params = {
-            "filename": "report",
-            # ...
-        }
-
-        # Call the validate_parameters function
-        validated_filename = validate_parameters(missing_cover_page_params)
-
-        # Assert that the function generates a random filename with ".pdf" extension
         self.assertTrue(validated_filename.endswith(".pdf"))
 
 
